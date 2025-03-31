3b709be80ef86ce8aa508b99c16f6cfaea5768ad
s16707 <s16707@pjwstk.edu.pl>
2025-03-31 20:31:21 +0200

    Dodanie klasy Container.

---
+++ Models/Container.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Containers.Exceptions;
+
+namespace Containers.Models
+{
+    public abstract class Container
+    {
+        private static int _idCounter = 1;
+        public string SerialNumber { get; }
+        public double MaxCapacity { get; }
+        public double CurrentLoad { get; private set; }
+
+        protected Container(string type, double maxCapacity)
+        {
+            SerialNumber = $"KON-{type}-{_idCounter++}";
+            MaxCapacity = maxCapacity;
+        }
+    }
+}
