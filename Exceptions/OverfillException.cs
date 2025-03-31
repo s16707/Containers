8180fa97a365165061ba6568b62b9593b6db523e
s16707 <s16707@pjwstk.edu.pl>
2025-03-31 19:54:04 +0200

    Obsługa wyjątku przy próbie przeładowania kontenera.

---
+++ Exceptions/OverfillException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Containers.Exceptions
+{
+    public class OverfillException : Exception
+    {
+        public OverfillException(string message) : base(message) { }
+    }
+}
