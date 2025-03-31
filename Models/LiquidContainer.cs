baef9a89d562a962bfff1fd0ce8633c30fa8d564
s16707 <s16707@pjwstk.edu.pl>
2025-03-31 20:51:06 +0200

    Dodanie kontenera na płyny.

---
+++ Models/LiquidContainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Containers.Interfaces;
+
+namespace Containers.Models
+{
+    public class LiquidContainer : Container, IHazardNotifier
+    {
+        public bool IsHazardous { get; }
+
+        public LiquidContainer(double maxCapacity, bool isHazardous) : base("L", maxCapacity)
+        {
+            IsHazardous = isHazardous;
+        }
+
+        public override void Load(double weight)
+        {
+            double capacityLimit = IsHazardous ? MaxCapacity * 0.5 : MaxCapacity * 0.9;
+
+            if (CurrentLoad + weight > capacityLimit)
+                NotifyHazard($"Attempted to load {weight}kg into hazardous container {SerialNumber}. Exceeds limit.");
+
+            base.Load(weight);
+        }
+
+        public void NotifyHazard(string message)
+        {
+            Console.WriteLine($"HAZAR NOTIFICATION: {message}");
+        }
+    }
+}
