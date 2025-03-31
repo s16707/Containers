822697a7975f144d826ec6f76be0fd8708166ca9
s16707 <s16707@pjwstk.edu.pl>
2025-03-31 21:56:09 +0200

    Dodano kontener z lodowka.

---
+++ Models/FridgeContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Containers.Utilities;
+
+namespace Containers.Models
+{
+    public class FridgeContainer : Container
+    {
+        public string ProductType { get; }
+        public double Temperature { get; }
+
+        public FridgeContainer(double maxCapacity, string productType, double temperature) : base("C", maxCapacity)
+        {
+            if (!ProductTemperatureData.RequiredTemperature.ContainsKey(productType))
+                throw new ArgumentException($"Unknown product type: {productType}");
+
+            if (temperature < ProductTemperatureData.RequiredTemperatures[productType])
+                throw new ArgumentException($"Temperature {temperature}C is too low for {productType}. Minimum required: {ProductTemperatureData.RequiredTemperatures[productType]}C");
+
+            ProductType = productType;
+            Temperature = temperature;
+        }
+    }
+}
