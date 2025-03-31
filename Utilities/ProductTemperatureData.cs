0b95936d4ec5302732a4ea660d046f92aaa0aca5
s16707 <s16707@pjwstk.edu.pl>
2025-03-31 19:36:47 +0200

    Poprawione wagi produktów.

--- Utilities/ProductTemperatureData.cs
+++ Utilities/ProductTemperatureData.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 
-
-public static class ProductTemperatureData
-{
-	public static readonly Dictionary<string, double> RequiredTemperatures = new()
+namespace Containers.Utilities
+{ 
+	public static class ProductTemperatureData
 	{
-		{ "Bananas", 13.3 },
-		{ "Bananas", 13.3 },
-		{ "Bananas", 13.3 },
-		{ "Bananas", 13.3 },
-		{ "Bananas", 13.3 },
-		{ "Bananas", 13.3 },
-		{ "Bananas", 13.3 },
-		{ "Bananas", 13.3 },
-		{ "Bananas", 13.3 },
-		{ "Bananas", 13.3 }
+		public static readonly Dictionary<string, double> RequiredTemperatures = new()
+		{
+			{ "Bananas", 13.3 },
+			{ "Chocolate", 18 },
+			{ "Fish", 2 },
+			{ "Meat", -15 },
+			{ "Ice cream", -18 },
+			{ "Frozen pizza", -30 },
+			{ "Cheese", 7.2 },
+			{ "Sausages", 5 },
+			{ "Butter", 20.5 },
+			{ "Eggs", 19 }
+
+		};
 
 	}
 }
