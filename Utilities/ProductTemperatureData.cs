f570b82401c44cc7fa27597a5593f150cb458961
s16707 <s16707@pjwstk.edu.pl>
2025-03-31 19:30:47 +0200

    Lista produktów i waga do sprawdzenia w kontenerze chłodniczym.

---
+++ Utilities/ProductTemperatureData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+public static class ProductTemperatureData
+{
+	public static readonly Dictionary<string, double> RequiredTemperatures = new()
+	{
+		{ "Bananas", 13.3 },
+		{ "Bananas", 13.3 },
+		{ "Bananas", 13.3 },
+		{ "Bananas", 13.3 },
+		{ "Bananas", 13.3 },
+		{ "Bananas", 13.3 },
+		{ "Bananas", 13.3 },
+		{ "Bananas", 13.3 },
+		{ "Bananas", 13.3 },
+		{ "Bananas", 13.3 }
+
+	}
+}
